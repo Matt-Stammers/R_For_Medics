abc5d23e9d71035bbddb1d2da921a5eb4ecd7848
Matt Stammers <matthewstammers@googlemail.com>
2017-08-06 12:55:57 +0100

    Update Basic_R_For_Medics.r

--- Basic_R_For_Medics.r
+++ Basic_R_For_Medics.r
@@ -1,1 +1,6 @@
-Welcome!
+# Welcome! Right this mini-intro code is just being created over the next few days, so watch this space!
+
+# Python is obviously a superior language in terms of computer science, generalisability and breadth of available packages,
+# frameworks and novel uses (for instance in the development of AI and neural networks python is becoming vastly superior.
+# However, R does have some advantages, certainly it is quicker and less verbose + in a data science / research context it is
+# very widely popular and as such any aspiring clinician data scientist should have at least a good foundational grasp of it.
